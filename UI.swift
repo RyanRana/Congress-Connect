f7f80eb852e983bd30ab5c92adf87dc8d057c7e0
Ryan Rana <39924576+RyanRana@users.noreply.github.com>
2022-08-16 09:52:55 -0400

    Add files via upload

---
+++ UI.swift
@@ -0,0 +1,123 @@
+HStack {
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//More Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Landing
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+}
+VStack {
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//More Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Landing
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+}
+ZStack {
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//More Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Information
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Enter
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+
+//Landing
+    RoundedRectangle(cornerRadius: 40)
+    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    .frame(width: 390, height: 844)
+}
